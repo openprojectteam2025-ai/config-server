049d3129ae8e40ef6df2951c8f3e62a88bf4a8a9
Alok <tech.alok93@gmail.com>
2025-10-15 18:42:21 +0530

    package rename

--- cloud_config_server/src/main/java/com/openproject/configserver/CloudConfigServerApplication.java
+++ cloud_config_server/src/main/java/com/openproject/configserver/CloudConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.openproject;
+package com.openproject.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
